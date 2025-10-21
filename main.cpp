bb5b4b3c143c0a2c57859d6a53de134f0241f895
linshu <3386549114@qq.com>
2025-10-21 19:45:42 +0800

    1.0

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "include/test.h"
+using namespace std;
+int main()
+{
+	cout<<"hello world";
+    testFunction();
+	return 0;
+}
---
+++ scr/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+#include"../include/test.h"
+using namespace std;
+
+void testFunction()
+{
+	cout << "This is a test function." << endl;
+}
